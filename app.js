5fbecb8145a62370dfcb63c9230cd6cffb76c0be
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-06-28 14:27:43 +0300

    pull request

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function watch(){
-    let date = new Date()
-         let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
-         let  minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
-         document.querySelector('.watch').innerHTML = hours + ':' + minutes
+function watch() {
+   let date = new Date()
+   let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
+   let minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
+   document.querySelector('.watch').innerHTML = hours + ':' + minutes
 }
 setInterval(watch, 1000);
 
-function date () {
-   let  dateyears = new Date ()
-   let dateUTC =  (dateyears.getFullYear())
+function date() {
+   let dateyears = new Date()
+   let dateUTC = (dateyears.getFullYear())
    document.querySelector('.date').innerHTML = `since.` + dateUTC
 }
 setInterval(date, 1000);
