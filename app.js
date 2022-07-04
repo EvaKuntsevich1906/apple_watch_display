fa1de48a874bf2fc4a8ecb1b00ee35e229acec24
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-07-04 21:26:00 +0300

    project with changes

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 function watch() {
-   let date = new Date()
-   let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
-   let minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
+   const date = new Date()
+   const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
+   const  minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
    document.querySelector('.watch').innerHTML = hours + ':' + minutes
+   const sinse = document.querySelector(`.sinse`)
+   const dateyears = new Date()
+   const dateUTC = (dateyears.getFullYear());
+   document.querySelector('.date').innerHTML =   dateUTC;
+
 }
 setInterval(watch, 1000);
-
-function date() {
-   let dateyears = new Date()
-   let dateUTC = (dateyears.getFullYear())
-   document.querySelector('.date').innerHTML = `since.` + dateUTC
-}
-setInterval(date, 1000);
