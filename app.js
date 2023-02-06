1fbc4f05f14b6ef0159e9e4c01a5563ed1b480ce
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-06 23:06:21 +0300

    fix

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function watch() {
+function watch () {
    const date = new Date()
    const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
    const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
    const seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-   document.querySelector('.watch').innerHTML = hours + ':' + minutes +':' + seconds
+   document.querySelector('.watch').innerHTML = hours + ':' + minutes +':' + seconds;
    document.querySelector('.date').innerHTML = date.getFullYear();
 }
 setInterval(watch, 1000);
