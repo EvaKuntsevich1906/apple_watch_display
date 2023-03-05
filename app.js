796cd3c33ea38f2a78c4b750cdc38afafdbdce55
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-03-05 22:55:11 +0300

    oop

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-function watch () {
-   const date = new Date()
-   const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
-   const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
-   const seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-   document.querySelector('.watch').innerHTML = hours + ':' + minutes +':' + seconds;
-   document.querySelector('.date').innerHTML = date.getFullYear();
+class Watch {
+   constructor() {
+      this.watch = document.querySelector(".block");
+      setInterval(this.doWatch, 1000);
+   }
+   
+   doWatch() {
+      const date = new Date()
+      const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
+      const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
+      const seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+      document.querySelector('.watch').innerHTML = hours + ':' + minutes + ':' + seconds;
+      document.querySelector('.date').innerHTML = date.getFullYear();
+   }
 }
-setInterval(watch, 1000);
+
+
+const watch = new Watch();
